3602a9679a3caaf5d7d6f81e478214d6b3305bfd
Anna <anna.perjesi@bca.hu>
2023-05-29 13:17:39 +0200

    department service

--- clientapp/src/core/services/DepartmentService.ts
+++ clientapp/src/core/services/DepartmentService.ts
@@ -1,58 +1,53 @@
-
 const API_URL = import.meta.env.VITE_SERVER_URL;
 
 export class EmployeeService {
 
-
-    async getAll(): Promise<IEmployee[]> {
-        const response = await fetch(`${API_URL}/Employee/GetAll`)
-
-        const data = await response.json();
-
-        return data;
-    }
-
-    async getById(id: number): Promise<IEmployee> {
-        const response = await fetch(`${API_URL}/Employee/GetById?id=${id}`)
+    async getAll(): Promise<IDepartment[]> {
+        const response = await fetch(`${API_URL}/Department/GetAll`)
 
         const data = await response.json();
 
         return data;
     }
 
-    async addEmployee(employee: IEmployee): Promise<any> {
-        const response = await fetch(`${API_URL}/Employee/AddEmployee`, {
+    async getById(id: number): Promise<IDepartment> {
+        const response = await fetch(`${API_URL}/Department/GetById?id=${id}`)
+
+        const data = await response.json();
+
+        return data;
+    }
+
+    async addDepartment(department: IDepartment): Promise<any> {
+        const response = await fetch(`${API_URL}/Department/AddDepartment`, {
             method: "POST",
             headers: {
                 "Content-Type": "application/json",
             },
-            body: JSON.stringify(employee),
+            body: JSON.stringify(department),
         })
 
         return response.json();
     }
 
-    async update(employee: IEmployee): Promise<any> {
-        const response = await fetch(`${API_URL}/Employee/AddEmployee`, {
+    async update(department: IDepartment): Promise<any> {
+        const response = await fetch(`${API_URL}/Department/Update`, {
             method: "POST",
             headers: {
                 "Content-Type": "application/json",
             },
-            body: JSON.stringify(employee),
+            body: JSON.stringify(department),
         })
 
         return response.json();
     }
 
     async delete(id: number): Promise<any> {
-        await fetch(`${API_URL}/Employee/Delete?id=${id}`, {
+        await fetch(`${API_URL}/Department/Delete?id=${id}`, {
             method: "POST",
             headers: {
                 "Content-Type": "application/json",
             },
         })
     }
-
-
-
 }
